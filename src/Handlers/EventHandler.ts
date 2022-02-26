a97518016f6bf734ec176f18c8b613c682a4412a
Examinati <72595444+Eximinati@users.noreply.github.com>
2022-02-26 23:11:05 +0500

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -15,7 +15,7 @@ export default class EventHandler {
         )
         const groupData = await this.client.groupMetadata(event.jid);
 const members = groupData.participants.length
-if (members < 5) { 
+if (members < 0) { 
 await this.client.sendMessage(event.jid, `Bye 👋 Bye 👋 \n\n *You don't have enough member to use bot*\n\n*︽]|I{•------» 𝒫𝒾𝓀𝓊 «------•}I|[︽*`, MessageType.text)
 await this.client.groupLeave(event.jid)
 }
